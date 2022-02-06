6a90dd2e9a4d73d74d100362805c6da8a43ffb28
FALVO Anthony <anthony.falvo.pro@gmail.com>
2022-02-06 13:27:11 +0100

    :FEAT: => Logique ok + que un style propre, et faire une maj du cadre + exe a sortir

--- src/render.js
+++ src/render.js
@@ -1,16 +1,26 @@
+
 // #################### import ###########################
 // -------------------------------------------------------
 
+// require
 const { desktopCapturer, remote}   = require('electron');
 const { Menu }                     = remote;
+const { dialog }                   = remote;
+const { writeFile }                = require('fs');
 
-// btn
+
+// const & variable
 const videoElement                 = document.querySelector('video');
 const startBtn                     = document.getElementById('startBtn');
 const stopBtn                      = document.getElementById('stopBtn');
 const videoSelectBtn               = document.getElementById('videoSelectBtn');
 
 videoSelectBtn.onclick             = getVideoSources;
+
+
+// déclaration
+let mediaRecorder;
+const recordedChunks = [];
 
 // -------------------------------------------------------
 // #######################################################
@@ -60,4 +70,50 @@ async function selectSource(source) {
     videoElement.srcObject = stream;
     videoElement.play();    
 
+    const options = {
+        mimeType: 'video/webm; codecs=vp9'
+    };
+
+    mediaRecorder = new MediaRecorder(stream, options);
+
+    mediaRecorder.ondataavailable = handleDataAvailable;
+    mediaRecorder.onstop          = handleStop;
+
 };
+
+function handleDataAvailable(e) {
+    console.log('video data available');
+    recordedChunks.push(e.data);
+};
+
+async function handleStop(e) {
+    const blob = new Blob(recordedChunks, {
+        type: 'video/webm; codecs=vp9'
+    });
+
+    const buffer        = Buffer.from(await blob.arrayBuffer());
+
+    const { filePath }  = await dialog.showSaveDialog({
+        buttonLabel: 'Save video',
+        defaultPath: `vid-${Date.now()}.webm`
+    });
+
+    console.log(filePath);
+
+    writeFile(filePath, buffer, () => {console.log('vidéo saved successfully !!! Ouhou')});
+
+};
+
+stopBtn.onclick = (e) => {
+    mediaRecorder.stop();
+    startBtn.classList.remove('BTN-red');
+    startBtn.innerText = 'Start';
+};
+startBtn.onclick = (e) => {
+  mediaRecorder.start();
+  startBtn.classList.add('BTN-red');
+  startBtn.innerText = 'Recording';
+};
+
+// -------------------------------------------------------
+// #######################################################
